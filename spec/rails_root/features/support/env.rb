303ea2a2df3028a44257c4a2351974a2df37d075
Mischa <f.mischa@gmail.com>
2008-12-18 02:50:48 -0800

    rename_email_spec_feature setup to the shorter cucumber.rb

--- spec/rails_root/features/support/env.rb
+++ spec/rails_root/features/support/env.rb
@@ -8,8 +8,5 @@ require 'webrat/rails'
 require 'cucumber/rails/rspec'
 require 'webrat/rspec-rails'
 
-require File.expand_path(File.dirname(__FILE__) + '../../../../../lib/email_spec/email_spec_feature_setup.rb')
+require File.expand_path(File.dirname(__FILE__) + '../../../../../lib/email_spec/cucumber.rb')
 
-
-# /Users/mischa/Projects/Rails/cycle/vendor/plugins/email-spec/lib/email_spec/email_spec_feature_setup.rb 
-# /Users/mischa/Projects/Rails/cycle/vendor/plugins/email-spec/spec/lib/email_spec/email_spec_feature_setup.rb 
