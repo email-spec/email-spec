2f0a2c1a171712a5608ab84779f58fbaa05d51eb
Mischa <f.mischa@gmail.com>
2008-12-17 18:45:15 -0800

    simplify

--- lib/email_spec_steps.rb
+++ lib/email_spec_steps.rb
@@ -2,53 +2,45 @@ Given "a clear email queue" do
   reset_mailer
 end
 
-# user perspective
-
-When %r{I open the email$} do
+# Action
+#
+When /^I open the email$/ do
   open_last_email.should_not be_nil
 end
 
-When %r{^'([^']*?)' opens? the email with subject "([^']*?)"$} do |email, subject|
-  open_email(email, :with_subject => subject).should_not be_nil
-end
-
-When %r{^'([^']*?)' opens? the email with text "([^']*?)"$} do |email, text|
-  open_email(email, :with_text => text).should_not be_nil
-end
-
-When %r{^.*?follows? "([^']*?)" in the email$} do |link_text|
+When /^I follow "(.*)" in the email$/ do |link_text|
   current_email.should_not be_nil
   link = parse_email_for_link(current_email, link_text)
   get(link)
 end
 
-When %r{^.*?clicks? "([^']*?)" in the email$} do |link_text|
+When /^I click "(.*)" in the email$/ do |link_text|
   current_email.should_not be_nil
   link = parse_email_for_link(current_email, link_text)
   get_via_redirect(link)
 end
 
-Then %r{^"([^']*?)" should receive (\d+) emails?$} do |email, n|
+
+# Verification
+Then /^"([^']*?)" should receive (\d+) emails?$/ do |email, n|
   unread_emails_for(email).size.should == n.to_i
 end
 
-Then %r{^"([^']*?)" should have (\d+) emails?$} do |email, n|
+Then /^"([^']*?)" should have (\d+) emails?$/ do |email, n|
   mailbox_for(email).size.should == n.to_i
 end
 
-# system perspective
-  
-Then %r{^an email should not be sent to "([^']*?)"$} do |email|
+Then /^"([^']*?)" should not receive an email$/ do |email|
   open_email(email).should be_nil
 end
 
-Then %r{^.*? should see "(.*)" in the subject$} do |text|
+Then /^I should see "(.*)" in the subject$/ do |text|
   raise ArgumentError, "To check the subject, you must first open an e-mail" if current_email.nil?
   current_email.should_not be_nil
   current_email.subject.should =~ Regexp.new(text)
 end
 
-Then %r{^.*? should see "(.*)" in the email$} do |text|
+Then /^I should see "(.*)" in the email$/ do |text|
   current_email.should_not be_nil
   current_email.body.should =~ Regexp.new(text)
 end
