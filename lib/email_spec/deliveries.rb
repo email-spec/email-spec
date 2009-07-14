3b479a093814619a6827c35e94d4714ea30b0260
Jakub Kosinski <jakub.kosinski@ghandal.net>
2009-07-14 22:01:27 +0800

    mailbox_for search for given email address in to, cc and bcc
    Signed-off-by: Ben Mabey <ben@benmabey.com>

--- lib/email_spec/deliveries.rb
+++ lib/email_spec/deliveries.rb
@@ -3,7 +3,7 @@ module EmailSpec
     def all_emails
       ActionMailer::Base.deliveries
     end
-    
+
     def last_email_sent
       ActionMailer::Base.deliveries.last || raise("No email has been sent!")
     end
@@ -11,18 +11,18 @@ module EmailSpec
     def reset_mailer
       ActionMailer::Base.deliveries.clear
     end
-    
+
     def mailbox_for(address)
       address = AddressConverter.instance.convert(address)
-      ActionMailer::Base.deliveries.select { |m| m.to.include?(address) }
+      ActionMailer::Base.deliveries.select { |m| m.to.include?(address) || m.bcc.include?(address) || m.cc.include?(address) }
     end
   end
-  
+
   module ARMailerDeliveries
     def all_emails
       Email.all.map{ |email| parse_to_tmail(email) }
     end
-    
+
     def last_email_sent
       if email = Email.last
         TMail::Mail.parse(email.mail)
@@ -34,22 +34,23 @@ module EmailSpec
     def reset_mailer
       Email.delete_all
     end
-    
+
     def mailbox_for(address)
       address = AddressConverter.instance.convert(address)
-      Email.all.select { |email| email.to.include?(address) }.map{ |email| parse_to_tmail(email) }
+      Email.all.select { |email| email.to.include?(address) || email.bcc.include?(address) || email.cc.include?(address) }.map{ |email| parse_to_tmail(email) }
     end
-    
+
     def parse_to_tmail(email)
       TMail::Mail.parse(email.mail)
-    end    
+    end
   end
-  
+
   module Deliveries
     if ActionMailer::Base.delivery_method == :activerecord
       include EmailSpec::ARMailerDeliveries
     else
       include EmailSpec::TestDeliveries
-    end    
+    end
   end
 end
+
