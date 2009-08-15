8e08446d3f3aedde33ab77d0c0a3bf0a9c70aba8
Ben Mabey <ben@benmabey.com>
2009-08-15 10:43:20 -0600

    trying to get runcoderun to find the cucumber bin...

--- features/rails_app_steps.rb
+++ features/rails_app_steps.rb
@@ -15,6 +15,7 @@ Given /^the example rails app is setup with the latest generators$/ do
 end
 
 When /^I run "([^\"]*)"$/ do |cmd|
+  cmd.gsub!('cucumber', `which cucumber`)
   root_dir = File.join(File.expand_path(File.dirname(__FILE__)), "..")
   Dir.chdir(File.join(root_dir, 'examples', 'rails_root')) do
     @output = `#{cmd}`
