4eb34d292f04b39b14e112e9546976755776a702
Ben Mabey <ben@benmabey.com>
2009-09-30 18:40:52 -0600

    move step defs to match convention

--- features/step_definitions/rails_app_steps.rb
+++ features/step_definitions/rails_app_steps.rb
@@ -1,7 +1,6 @@
 require 'fileutils'
 
 Given /^the example rails app is setup with the latest generators$/ do
-  root_dir = File.join(File.expand_path(File.dirname(__FILE__)), "..")
 
   email_specs_path = "#{root_dir}/examples/rails_root/features/step_definitions/email_steps.rb"
   FileUtils.rm(email_specs_path) if File.exists?(email_specs_path)
@@ -16,7 +15,6 @@ end
 
 When /^I run "([^\"]*)" in the rails root$/ do |cmd|
   cmd.gsub!('cucumber', "#{Cucumber::RUBY_BINARY} #{Cucumber::BINARY}")
-  root_dir = File.join(File.expand_path(File.dirname(__FILE__)), "..")
   Dir.chdir(File.join(root_dir, 'examples', 'rails_root')) do
     @output = `#{cmd}`
   end
--- features/support/sinatra_app_steps.rb
+++ features/support/sinatra_app_steps.rb
@@ -6,7 +6,6 @@ When /^I run "([^\"]*)" in the sinatra root$/ do |cmd|
   Given "the example rails app is setup with the latest generators"
   When 'I run "rake db:migrate RAILS_ENV=test" in the rails root'
   cmd.gsub!('cucumber', "#{Cucumber::RUBY_BINARY} #{Cucumber::BINARY}")
-  root_dir = File.join(File.expand_path(File.dirname(__FILE__)), "..")
   Dir.chdir(File.join(root_dir, 'examples', 'sinatra')) do
     @output = `#{cmd}`
   end
