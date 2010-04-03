ab81201ea4b84fce44caeda00a03daace1adec65
Maxim Chernyak <max@bitsonnet.com>
2010-04-03 15:05:56 -0400

    More RSpec2 compatibility.

--- spec/email_spec/helpers_spec.rb
+++ spec/email_spec/helpers_spec.rb
@@ -17,7 +17,7 @@ describe EmailSpec::Helpers do
 
     it "causes a spec to fail if the body doesn't contain the text specified to click" do
       email = stub(:has_body_text? => false)
-      lambda { parse_email_for_link(email, "non-existent text") }.should raise_error(Spec::Expectations::ExpectationNotMetError)
+      lambda { parse_email_for_link(email, "non-existent text") }.should raise_error(Rspec::Expectations::ExpectationNotMetError)
     end
   end
 end
