402ce97176511b5f67c5a6a7d68986cacf21d428
Maxim Chernyak <max@bitsonnet.com>
2010-04-03 15:05:56 -0400

    More RSpec2 compatibility.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,7 @@ require 'rubygems'
 require 'action_mailer'
 require 'rspec'
 require File.expand_path(File.dirname(__FILE__) + '/../lib/email_spec.rb')
+
+Rspec.configure do |config|
+  config.mock_with :rspec
+end
