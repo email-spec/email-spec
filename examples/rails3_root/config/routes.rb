a761a8276225ea96fb9f28de19d9c090cc3b5fde
Maxim Chernyak <max@bitsonnet.com>
2010-04-05 19:07:18 -0400

    Add cucumber dependencies to rails3 example app.

--- examples/rails3_root/config/routes.rb
+++ examples/rails3_root/config/routes.rb
@@ -1,8 +1,6 @@
 Rails3Root::Application.routes.draw do |map|
-
   root :to => "welcome#index"
   match "/confirm" => "welcome#confirm", :as => :confirm_account
   match "/newsletter" => "welcome#newsletter", :as => :request_newsletter
   match "/attachments" => "welcome#attachments", :as => :request_attachments
-
 end
