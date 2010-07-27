3262bc5486a345cd94ad450d1be76c7bced81d04
Patrick Muldoon <doon@inoc.net>
2010-07-26 21:20:53 -0400

    updating to rails3.0.0.rc

--- examples/rails3_root/config/environments/development.rb
+++ examples/rails3_root/config/environments/development.rb
@@ -16,4 +16,5 @@ Rails3Root::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  config.active_support.deprecation = :stderr
 end
