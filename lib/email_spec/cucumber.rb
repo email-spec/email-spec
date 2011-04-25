313e3cc5d23be0be8e7cada0def5ec6571fa5a4b
Donald Piret <donald@donaldpiret.com>
2011-04-25 01:13:18 -0700

    I don't like email_spec being too smart about setting my delivery method to test, I want to use mocksmtp so want to be able to still have an smtp delivery method

--- lib/email_spec/cucumber.rb
+++ lib/email_spec/cucumber.rb
@@ -2,9 +2,10 @@
 
 # Global Setup
 if defined?(ActionMailer)
-  unless [:test, :activerecord, :cache, :file].include?(ActionMailer::Base.delivery_method)
-    ActionMailer::Base.delivery_method = :test
-  end
+  ## I don't like email_spec being too smart about this, I want to use mocksmtp so want to be able to still have an smtp delivery method
+  #unless [:test, :activerecord, :cache, :file].include?(ActionMailer::Base.delivery_method)
+  #  ActionMailer::Base.delivery_method = :test
+  #end
   ActionMailer::Base.perform_deliveries = true
 
   Before do
