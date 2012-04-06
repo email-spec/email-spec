27fe92edaf02c31db9358a7bdb30030b48fe6456
hube <stargeneral@gmail.com>
2012-04-06 10:08:08 -0500

    Correct a comment.

--- rails_generators/email_spec/templates/email_steps.rb
+++ rails_generators/email_spec/templates/email_steps.rb
@@ -26,7 +26,7 @@
 
 module EmailHelpers
   def current_email_address
-    # Replace with your a way to find your current email. e.g @current_user.email
+    # Replace with your way to find your current email (e.g @current_user.email).
     # last_email_address will return the last email address used by email spec to find an email.
     # Note that last_email_address will be reset after each Scenario.
     last_email_address || "example@example.com"
