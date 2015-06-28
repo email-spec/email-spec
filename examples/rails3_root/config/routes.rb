a39adcf9c1e668169e21ca3cbafdc3669829415f
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-06-28 02:16:51 -0300

    Renamed as rails4_root

--- examples/rails3_root/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails3Root::Application.routes.draw do
-  root :to => "welcome#index"
-  get "/confirm" => "welcome#confirm", :as => :confirm_account
-  get "/newsletter" => "welcome#newsletter", :as => :request_newsletter
-  get "/attachments" => "welcome#attachments", :as => :request_attachments
-  post "/welcome/signup" => "welcome#signup"
-end
