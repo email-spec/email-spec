a5ccabb787aebf683679fcc8acdcf8476e88da2a
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-06-28 02:09:23 -0300

    Upgrade routes to Rails 4+

--- examples/rails3_root/config/routes.rb
+++ examples/rails3_root/config/routes.rb
@@ -1,7 +1,7 @@
 Rails3Root::Application.routes.draw do
   root :to => "welcome#index"
-  match "/confirm" => "welcome#confirm", :as => :confirm_account
-  match "/newsletter" => "welcome#newsletter", :as => :request_newsletter
-  match "/attachments" => "welcome#attachments", :as => :request_attachments
-  match "/welcome/signup" => "welcome#signup"
+  get "/confirm" => "welcome#confirm", :as => :confirm_account
+  get "/newsletter" => "welcome#newsletter", :as => :request_newsletter
+  get "/attachments" => "welcome#attachments", :as => :request_attachments
+  post "/welcome/signup" => "welcome#signup"
 end
