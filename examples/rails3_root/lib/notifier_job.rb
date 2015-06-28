45002e2abe1d8016bd76fc5047008ac0a155f36c
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-06-28 02:11:56 -0300

    Use #deliver_now instead of #deliver

--- examples/rails3_root/lib/notifier_job.rb
+++ examples/rails3_root/lib/notifier_job.rb
@@ -1,5 +1,5 @@
 class NotifierJob < Struct.new(:notifier_method,:username,:name)
   def perform
-    UserMailer.send(notifier_method,username, name).deliver
+    UserMailer.send(notifier_method,username, name).deliver_now
   end
 end
