42ff8c8e34dcab12e43fe70783d04ba6a55c143f
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2015-06-28 02:17:34 -0300

    Renamed as rails4_root

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -24,7 +24,7 @@ Given /^the (\w+) app is setup with the latest generators$/ do |app_name|
   email_specs_path = File.join(app_dir,'features','step_definitions','email_steps.rb')
   FileUtils.rm(email_specs_path) if File.exists?(email_specs_path)
 
-  if app_name == 'rails3'
+  if app_name == 'rails4'
     #Testing using the gem
     #make sure we are listed in the bundle
     Dir.chdir(app_dir) do
@@ -45,7 +45,7 @@ When /^I run "([^\"]*)" in the (\w+) app$/ do |cmd, app_name|
   app_path = File.join(root_dir, 'examples', "#{app_name}_root")
   app_specific_gemfile = File.join(app_path,'Gemfile')
   Dir.chdir(app_path) do
-    #hack to fight competing bundles (email specs vs rails3_root's
+    #hack to fight competing bundles (email specs vs rails4_root's
     if File.exists? app_specific_gemfile
       orig_gemfile = ENV['BUNDLE_GEMFILE']
       ENV['BUNDLE_GEMFILE'] = app_specific_gemfile
