cc2b8b30034374b627a71cb6bfd9b0b68269bb8e
Koichi ITO <koic.ito@gmail.com>
2017-02-28 11:51:30 +0900

    Suppress `warning: mismatched indentations at 'end' with 'if' at 100`

--- lib/email_spec/helpers.rb
+++ lib/email_spec/helpers.rb
@@ -102,7 +102,7 @@ module EmailSpec
       elsif email.nil?
         error = "#{opts.keys.first.to_s.gsub("_", " ").downcase unless opts.empty?} #{('"' + opts.values.first.to_s + '"') unless opts.empty?}"
         raise EmailSpec::CouldNotFindEmailError, "Could not find email #{error} in the mailbox for #{current_email_address}. \n Found the following emails:\n\n #{all_emails.to_s}"
-       end
+      end
       email
     end
 
